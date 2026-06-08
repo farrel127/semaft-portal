69cc6c13b1da797b69c7b63eb9019db506acbdfa
Naufal Farrel <farreln130@gmail.com>
2026-06-08 19:35:13 +0700

    Upload ulang file gambar favicon ke public

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
 
         <!-- Fonts & Icons -->
         <link rel="preconnect" href="https://fonts.bunny.net">
-        <link rel="icon" href="{{ asset('favicon.png') }}?v=2" type="image/png">
+        <link rel="icon" href="{{ asset('images/sema.png') }}?v=3" type="image/png">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 
